effcd6ca7e56ddd6b9218028352d19bdb7a76ac5
Pavel Anihimovsky <p.anihimovsky@a1qa.com>
2019-04-29 09:15:07 +0300

    Add retrieve function to wait

--- test/wait.spec.js
+++ test/wait.spec.js
@@ -13,4 +13,14 @@ describe('Wait util tests', () => {
         const wait = new Wait();
         return wait.forTrue(() => false, 5, 1000, 0).then(result => { assert.isFalse(result); });
     });
+
+    it('should wait for "true"', () => {
+        const wait = new Wait();
+        return wait.forFalse(() => true, 5, 1000, 0).then(result => { assert.isFalse(result); });
+    });
+
+    it('should wait for "false"', () => {
+        const wait = new Wait();
+        return wait.forFalse(() => false, 5, 1000, 0).then(result => { assert.isTrue(result); });
+    });
 });
